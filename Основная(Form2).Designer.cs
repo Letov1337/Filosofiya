4341ec2d17f2b684adc5dae4e88fad2649476f23
Chekhov <pavel.miridonov123@gmail.com>
2021-02-25 21:39:58 +0400

    Anek

--- Основная(Form2).Designer.cs
+++ Основная(Form2).Designer.cs
@@ -31,6 +31,7 @@ namespace Filosofiya
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -53,11 +54,22 @@ namespace Filosofiya
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // основное1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "основное1";
@@ -71,5 +83,6 @@ namespace Filosofiya
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Основная(Form2).cs
+++ Основная(Form2).cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace Filosofiya
 {
@@ -48,6 +49,12 @@ namespace Filosofiya
             
             
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            const string url = "https://baneks.site/%D0%B2%D0%B5%D0%BB%D0%B8%D0%BA%D0%B8%D0%B9-%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84-%D1%84%D1%80%D0%B8%D0%B4%D1%80%D0%B8%D1%85-%D0%BD%D0%B8%D1%86%D1%88%D0%B5-%D0%B1%D1%8B%D0%BB-%D0%B4%D0%BE%D0%B1%D1%80%D1%8B%D0%B9-%D0%B8-%D0%B2%D0%B5%D1%81%D0%B5%D0%BB%D1%8B%D0%B9/?p=2";
+            Process.Start(url);
         }
     }
 }
